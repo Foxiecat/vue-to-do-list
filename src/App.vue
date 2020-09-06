917ca4aca0af6be9d0df2e1c03af6847efe4333c
Danica Ellefsen Kvilhaug <danicaglox@gmail.com>
2020-09-07 00:22:20 +0200

    To do list in Vue.js

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,53 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <Header />
+    <AddTodo />
+    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Todos from "./components/Todos";
+import AddTodo from "./components/AddTodo";
+import Header from "./components/layout/header"
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Todos,
+    Header,
+    AddTodo
+  },
+  data() 
+  {
+    return {
+      todos: [
+        {
+          id: 1,
+          title: "To-do One",
+          completed: false
+        },
+        {
+          id: 2,
+          title: "To-do Two",
+          completed: false
+        },
+        {
+          id: 3,
+          title: "To-do Three",
+          completed: false
+        }
+      ]
+    }
+  },
+  methods:
+  {
+    deleteTodo(id)
+    {
+      this.todos = this.todos.filter(todo => todo.id !== id);
+    }
   }
 }
 </script>
