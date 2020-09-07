e5fccca326ae41e6db5e30055d35dba0042458e1
Danica Ellefsen Kvilhaug <65165280+Gloxie@users.noreply.github.com>
2020-09-07 12:05:39 +0200

    Finished
    Might remake this so I can put it on a portfolio

--- dist/js/about.8e579ef1.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(t,e,n){"use strict";n.r(e);var s=function(){var t=this,e=t.$createElement;t._self._c;return t._m(0)},a=[function(){var t=this,e=t.$createElement,n=t._self._c||e;return n("div",{staticClass:"about"},[n("h1",[t._v("About")]),n("p",[t._v("This is the To-Do List app v1.0.0. It is part of a Vue course on YT")])])}],o=n("2877"),u={},i=Object(o["a"])(u,s,a,!1,null,null,null);e["default"]=i.exports}}]);
-//# sourceMappingURL=about.8e579ef1.js.map
